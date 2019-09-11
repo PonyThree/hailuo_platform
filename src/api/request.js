6f7b6c5600a762bbc8e044ce8e4e463b05477cfc
guowei <guowei@eatbeancar.com>
2019-09-11 16:15:11 +0800

    9.11 16:14平台端修改bug后提交

--- src/api/request.js
+++ src/api/request.js
@@ -10,8 +10,8 @@
 // const request='http://192.168.1.34:7999';//汪安兵
 // const request='http://192.168.1.32:7999';//徐联林
 // const request='http://192.168.1.39:7999'; //张东
-// const request='http://192.168.1.144:7999'; //刘星
-const request='http://192.168.1.101:7999'; //测试
+const request='http://192.168.1.144:7999'; //刘星
+// const request='http://192.168.1.101:7999'; //测试
 // const request="https://hailuo123.com/api";
 export default {
 	testUrl:request
