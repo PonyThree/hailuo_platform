6d314f1ac211e5946f3cac7dc8b5a83fc7b0c5ca
guowei <guowei@eatbeancar.com>
2019-12-27 10:46:01 +0800

    修改了的首页banner

--- src/api/request.js
+++ src/api/request.js
@@ -1,16 +1,16 @@
 
 // const request='http://192.168.1.34:7999';//汪安兵
-const request='http://192.168.1.32:7999';//徐联林
+// const request='http://192.168.1.32:7999';//徐联林
 // const request='http://192.168.1.39:7999'; //张东
 // const request='http://192.168.1.144:7999'; //刘星
-// const request='http://192.168.1.102:7999'; //测试
+const request='http://192.168.1.102:7999'; //测试
 // const request='http://192.168.1.140:7999'; //陳星宇
 // const request='http://192.168.1.92:7999'; //陳星宇
 // const request="https://hailuo123.com/api"; //正式服
 // const request='http://118.25.60.79:7999'; //预发布
 // const request='https://pre.api.hailuo123.com/api'
 // const request='https://www.hailuozhaowei.com/api'
-// const request='https://hellohailuo.com/api'
+// const request='https://hellohailuo.com/api'//预发布
 export default {
 	testUrl:request
 }
