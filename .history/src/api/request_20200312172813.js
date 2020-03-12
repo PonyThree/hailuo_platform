0c3b229e3a1b25120a700bcc39f3f2693c0c78b0
guowei <guowei@eatbeancar.com>
2020-03-12 17:29:20 +0800

    no message

---
+++ .history/src/api/request_20200312172813.js
@@ -0,0 +1,24 @@
+/*
+ * @Author: your name
+ * @Date: 2019-09-11 16:11:04
+ * @LastEditTime: 2020-03-12 17:21:05
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: \海螺平台github版本\src\api\request.js
+ */
+// const request='http://192.168.1.34:7999';//汪安兵
+// const request='http://192.168.1.32:7999';//徐联林
+// const request='http://192.168.1.39:7999'; //张东
+// const request='http://192.168.1.144:7999'; //刘星
+// const request='http://192.168.1.102:7999'; //测试
+// const request='http://192.168.1.140:7999'; //陳星宇
+// const request='http://192.168.1.92:7999'; //陳星宇
+// const request="https://hailuo123.com/api"; //正式服
+// const request='http://118.25.60.79:7999'; //预发布
+// const request='https://pre.api.hailuo123.com/api'
+// const request='https://www.hailuozhaowei.com/api'
+const request = "https://apiv2.hailuo123.com/api" //线上新域名
+// const request = 'https://hellohailuo.com/api' //预发布
+export default {
+	testUrl: request
+}
